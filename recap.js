52e6f98e8f92d18d6d6d87e445400cab7b01c1f7
RabeyaEty01 <etyrabeya01@gmail.com>
2021-08-05 12:13:04 +0600

    recap

---
+++ recap.js
@@ -0,0 +1,46 @@
+var time = '10:15pm'; //string
+var bookPrice = 150; //numeric or number type
+var isWhiteColor = false; //boolean type
+
+
+//array
+var partners = ['noman', 'mahbub', 'shafin', 'ety', 'tisha'];
+
+var elementCount = partners.length;
+var nomanIndex = partners.indexOf('noman');
+partners.push('rifa');
+partners.pop();
+
+//conditionals
+
+if (bookPrice < 120) {
+    console.log('I will buy this book');
+
+}
+else {
+    console.log('Mama kichu discount then na!!');
+}
+
+//while loop
+
+var i = 0;
+while (i <= 20) {
+    //do some work
+    i++;
+}
+
+//for loop
+for(var i=0; i<=17; i++)
+{
+
+}
+//function
+function isMoonUp(time){
+    if(time>= 19 && time <=5)
+    {
+        return true;
+    }
+    return false;
+}
+
+var moonStatus =isMoonUp(21);
